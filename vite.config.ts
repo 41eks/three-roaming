3ef83997bac60db54e76bc1c02c1006946fda1d6
41eks <koukyokutou123@gmail.com>
2026-05-29 16:44:27 +0900

    deploy

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+
+
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // 将 'your-repo-name' 替换为你真实的 GitHub 仓库名称
+  // 例如你的仓库是 https://github.com/john/my-project，这里就写 '/my-project/'
+  base: '/three-roaming/',
+})
